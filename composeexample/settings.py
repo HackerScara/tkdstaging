deaac0d07feaf47877c037c1d5700da7e04916dc
LoganScara <loganscara@pop-os.localdomain>
2020-03-09 21:26:33 -0500

    add admin page writes to db

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'staging.apps.StagingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- staging/models.py
+++ staging/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class stage(models.Model):
+    ring = models.CharField(max_length=2)
+    age = models.CharField(max_length=50)
+    rank = models.CharField(max_length=50)
+    sex = models.CharField(max_length=10)
+    type = models.CharField(max_length=50)
+    def __str__(self):
+        return self.ring
