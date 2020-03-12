cfd3ee71b2950157ae5a0903b31f9bbfbdde2d4a
LoganScara <loganscara@pop-os.localdomain>
2020-03-11 20:06:33 -0500

    add initial view

--- staging/views.py
+++ staging/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import stage
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the staging index.")
+    stages = stage.objects.all()
+    context = {'rings': stages}
+    return render(request, 'ring_template.html', context)
